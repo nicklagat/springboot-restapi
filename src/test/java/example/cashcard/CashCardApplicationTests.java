df9c1f7616949794da96a8ebdec7043dc7af1716
nicklagat <nicksonlagatk@gmail.com>
2023-08-29 02:35:44 +0300

    module 1- creating RESTful Endpoints

---
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,15 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
